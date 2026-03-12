fd08c12ce7afec7171a9f2f907ed6ce3b94b9301
Dev Sahu <devsahu7180@gmail.com>
2026-03-12 16:26:05 +0530

    Delete config.py
    added by mistake

--- config.py
+++
@@ -1,6 +0,0 @@
-import os
-basedir = os.path.abspath(os.path.dirname(__file__))
-class Config:
-    SECRET_KEY=os.environ.get("'SECRET_KEY") or "123456789"
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
-        'sqlite:///' + os.path.join(basedir, 'app.db')
