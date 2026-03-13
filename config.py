034b11af35ea595ef312d307f6fd3e408667a069
devsahu8 <devsahu7180@gmail.com>
2026-03-14 01:39:09 +0530

    please work

--- config.py
+++ config.py
@@ -4,5 +4,5 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 
 class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-will-never-guess'
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
+    SQLALCHEMY_DATABASE_URI = os.environ.get('POSTGRES_URL') or \
         'sqlite:///' + os.path.join(basedir, 'app.db')
