25c122d24c741bb042cc0befe31f561f8e9387c7
devsahu8 <devsahu7180@gmail.com>
2026-03-14 01:47:04 +0530

    please pplease worl

--- casino.py
+++ casino.py
@@ -4,6 +4,12 @@ import sqlalchemy.orm as so
 from app import app, db
 from app.models import User
 
+from app import create_app, db
+app = create_app()
+
+with app.app_context():
+    db.create_all() # This creates your user/score tables in the cloud
+
 if __name__ == "__main__":
     app.run()
 
--- config.py
+++ config.py
@@ -1,8 +1,11 @@
 import os
 basedir = os.path.abspath(os.path.dirname(__file__))
 
-
 class Config:
-    SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-will-never-guess'
+    SECRET_KEY = os.environ.get('SECRET_KEY') or 'king-of-kings-key'
+    
+    # THE FIX: Try to find the Postgres URL from Vercel first
     SQLALCHEMY_DATABASE_URI = os.environ.get('POSTGRES_URL') or \
         'sqlite:///' + os.path.join(basedir, 'app.db')
+    
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
