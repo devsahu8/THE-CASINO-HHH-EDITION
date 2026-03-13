8f67808beb4b3c11f89ab567ffc8313d43173868
devsahu8 <devsahu7180@gmail.com>
2026-03-14 01:04:21 +0530

    123

--- casino.py
+++ casino.py
@@ -4,6 +4,9 @@ import sqlalchemy.orm as so
 from app import app, db
 from app.models import User
 
+if __name__ == "__main__":
+    app.run()
+
 @app.shell_context_processor
 def make_shell_context():
     return {'sa': sa, 'so': so, 'db': db, 'User': User}
